e9c55154e440cb6bc9a4aa344f0bd743dc01b7ac
Shreyas Shinde <87196415+Shreyas24102002@users.noreply.github.com>
2023-04-30 23:17:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ require("dotenv").config();
 const app = express();
 const router = require("./routes/contactRoutes");
 
+const cors = require("cors");
+
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.use("/api/contact", router);
 app.use("/api/volunteer", router);
