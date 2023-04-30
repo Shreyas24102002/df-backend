f329b83f3fac3e5d58bb6f969ca41c210982cd17
Shreyas Shinde <87196415+Shreyas24102002@users.noreply.github.com>
2023-04-30 20:11:16 +0530

    Update Volunteer.js

--- models/Volunteer.js
+++ models/Volunteer.js
@@ -5,44 +5,40 @@ const Schema = mongoose.Schema;
 const volunteerSchema = new Schema({
   fullName: {
     type: String,
-    required: true,
   },
   address: {
     type: String,
-    required: true,
   },
   contactNo: {
     type: Number,
     // maxlength: 10,
-    required: true,
   },
   email: {
     type: String,
   },
   dob: {
     type: String,
-    required: true,
   },
   bloodGroup: {
     type: String,
   },
   education: {
     type: String,
-    required: true,
   },
   business: {
     type: String,
-    required: true,
   },
   hobbies: {
     type: String,
   },
-  experience: {
+  gender: {
+    type: String,
+  },
+  social: {
     type: String,
   },
   help: {
     type: String,
-    required: true,
   },
 });
 
